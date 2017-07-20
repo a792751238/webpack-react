6abdb8dc861f6c665704dfd25c0076fcb57bbf74
easterCat <fuhuodemao@163.com>
2017-07-20 16:08:27 +0800

    add some module

---
+++ app/components/Header.js
@@ -0,0 +1,13 @@
+/**
+ * Created by easterCat on 2017/7/20.
+ */
+
+import React, {Component} from 'react';
+
+function Header(props) {
+    return (
+        <div className="header">this is header</div>
+    )
+}
+
+export default Header;
--- app/main.js
+++ app/main.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import {render} from 'react-dom';
 import './index.css';
-import Main from './components/Main';
+import App from './components/App';
 
 const root = document.getElementById('root');
 
-render(<div>hello world!</div>, root);
+render(<App/>, root);
