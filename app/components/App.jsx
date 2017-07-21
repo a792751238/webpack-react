5431989606e55c1a29d7cf5e301f8e34f2eec13a
easterCat <fuhuodemao@163.com>
2017-07-21 17:17:54 +0800

    终于可以跑了3

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -3,6 +3,11 @@ import Header from './Header';
 import Body from './Body';
 
 class App extends React.Component {
+    constructor() {
+        super();
+
+        console.dir(Promise);
+    }
     render() {
         return (
             <div>Hello World!123123
