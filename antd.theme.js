5c65160da4ff56600e1afb2ff97caa496a81317c
easterCat <fuhuodemao@163.com>
2018-06-25 10:33:25 +0800

    update

---
+++ antd.theme.js
@@ -0,0 +1,8 @@
+/**
+ * Created by easterCat on 2017/11/20.
+ * https://github.com/ant-design/ant-design/blob/master/components/style/themes/default.less
+ */
+module.exports = {
+    "@primary-color": "#0089CF",
+
+};
