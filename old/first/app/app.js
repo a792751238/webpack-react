972dbd2382736510a30f4ba478629348c1d48ee0
easterCat <fuhuodemao@163.com>
2018-06-27 14:19:46 +0800

    'addcss'

--- old/first/app/app.js
+++ old/first/app/app.js
@@ -2,6 +2,7 @@
  * Created by easterCat on 2018/6/27.
  */
 import './index.html';
+import './app.css';
 import React from 'react';
 import ReactDom from 'react-dom';
 import App from './components/App.jsx';
