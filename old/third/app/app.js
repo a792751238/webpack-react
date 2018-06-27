a06571fe4cb49d20648042ac46f491f7eebe8c4a
easterCat <fuhuodemao@163.com>
2018-06-27 17:24:21 +0800

    'redux'

--- old/third/app/app.js
+++ old/third/app/app.js
@@ -7,11 +7,15 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import App from './components/App.jsx';
 import {BrowserRouter, Route} from 'react-router-dom';
+import {Provider} from 'react-redux';
+import store from './store/store';
 
 ReactDom.render(
-    <BrowserRouter>
-        <Route path="/" component={App}/>
-    </BrowserRouter>
+    <Provider store={store}>
+        <BrowserRouter>
+            <Route path="/" component={App}/>
+        </BrowserRouter>
+    </Provider>
     ,
     document.getElementById('root')
 );
